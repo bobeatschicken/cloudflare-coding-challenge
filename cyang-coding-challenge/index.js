5d1a46f1ead20213e6b7bbfdc1dd2d960426a6b2
Christopher Yang <christopheryang@Christophers-MacBook-Pro.local>
2020-04-14 01:41:09 -0700

    fetches variants from api and saves to local variable

--- cyang-coding-challenge/index.js
+++ cyang-coding-challenge/index.js
@@ -5,7 +5,22 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
+
+async function fetchVariants() {
+  var variants;
+  fetch("https://cfw-takehome.developers.workers.dev/api/variants")
+    .then(res => res.json())
+    .then(data => variants = data)
+    .then(() => console.log("first ", variants))
+  console.log("second ", variants);
+}
 async function handleRequest(request) {
+  var variants;
+  await fetch("https://cfw-takehome.developers.workers.dev/api/variants")
+    .then(res => res.json())
+    .then(data => variants = data)
+    .then(() => console.log("first ", variants))
+  console.log("second", variants);
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
